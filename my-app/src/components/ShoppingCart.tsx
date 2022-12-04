06c9192049764e474394cad00dd6688a37305bb0
dimachilaia <chilaiadima@gmail.com>
2022-12-04 13:51:25 +0400

    created shoppingCart component

--- my-app/src/components/ShoppingCart.tsx
+++ my-app/src/components/ShoppingCart.tsx
@@ -1,17 +1,24 @@
 import React from "react";
-import { Offcanvas, OffcanvasHeader } from "react-bootstrap";
+import { Offcanvas, OffcanvasHeader, Stack } from "react-bootstrap";
 import { useShoppingCart } from "../context/ShoppingCartContext";
-
+import CartItem from "./CartItem";
 type ShoppingCartProps = {
   isOpen: boolean;
 };
 const ShoppingCart = ({ isOpen }: ShoppingCartProps) => {
-  const { closeCart } = useShoppingCart();
+  const { closeCart, cartItems } = useShoppingCart();
   return (
     <Offcanvas show={isOpen} placement="end" onHide={closeCart}>
       <Offcanvas.Header closeButton>
         <Offcanvas.Title>Cart</Offcanvas.Title>
       </Offcanvas.Header>
+      <Offcanvas.Body>
+        <Stack gap={3}>
+          {cartItems.map((item) => {
+            return <CartItem key={item.id} {...item} />;
+          })}
+        </Stack>
+      </Offcanvas.Body>
     </Offcanvas>
   );
 };
